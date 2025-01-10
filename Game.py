dc0ab59824f4d3efb0028e0d5e3859daccdbee4a
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2025-01-10 08:53:48 +0500

    Продолжение работы 31.12.2024

--- Game.py
+++ Game.py
@@ -3,6 +3,89 @@ import sys
 import os
 
 
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, name_img, pos_x, pos_y):
+        super().__init__(coin_group, all_sprites)
+        self.image = load_image(name_img)
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 5, tile_height * pos_y + 5)
+        self.image.set_colorkey((255, 255, 255))
+
+
+class Strelka(pygame.sprite.Sprite):
+    def __init__(self, name_img, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image(name_img)
+        self.rect = self.image.get_rect().move(
+            pos_x, image_level_height * pos_y
+        )
+        # self.rect.x = pos_x
+        # self.rect.y = pos_y
+        self.image.set_colorkey((255, 255, 255))
+
+
+class Levelnum(pygame.sprite.Sprite):  # класс спрайтов  для "выбор уровня"
+    def __init__(self, level_type, pos_x, pos_y):
+        super().__init__(levels_group, all_sprites)
+        self.image = level_images[level_type]
+        self.rect = self.image.get_rect().move(pos_x, image_level_height * pos_y)
+
+
+def image_gen_level(level_img):  # генерируем спрайты "выбор уровня Level n"
+    x, y = 20, 1
+    for level_name in level_img:
+        y += 1
+        Levelnum(level_name, x, y)
+    return
+
+
+def chioce_level_screen():
+    # группа спрайтов стрелки для выбора
+    strelka_group = pygame.sprite.Group()
+    coord_x = 255 + 20
+    coord_y = 2
+    dy = 1  # выбранный уровень
+    strelka_level = Strelka('strelka.png', coord_x, coord_y)
+    strelka_group.add(strelka_level)
+    play_game = Strelka('play.png', width - 200, 11)
+    strelka_group.add(play_game)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    terminate()
+                if event.key == pygame.K_DOWN and 0 < dy < 9:
+                    strelka_level.rect.y += image_level_height
+                    dy += 1
+                elif event.key == pygame.K_UP and 1 < dy < 10:
+                    strelka_level.rect.y -= image_level_height
+                    dy -= 1
+                elif event.key == pygame.K_SPACE:
+                    return dy - 1
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if play_game.rect.collidepoint(event.pos):
+                    return dy - 1  # начинаем игру
+        screen.fill(pygame.Color(0, 0, 0))
+        # отрисовка фона старт
+        fon = pygame.transform.scale(load_image("fon.png"), (width, height))
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 45)
+        intro_text = "Выбери уровень"
+        text_coord = 80
+        string_rendered = font.render(intro_text, 1, pygame.Color("black"))
+        intro_rect = string_rendered.get_rect()
+        intro_rect.top = text_coord
+        intro_rect.x = 20
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+        # отрисовка фона финиш
+        levels_group.draw(screen)
+        strelka_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
@@ -15,22 +98,8 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5
+            tile_width * pos_x + 8, tile_height * pos_y + 0
         )
-
-
-class Camera:
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
 
 
 def load_image(name, color_key=None):
@@ -56,7 +125,7 @@ def terminate():  # завершение игры
 
 
 def start_screen():
-    pygame.mixer.music.load('data/Mario.mp3')  # подгружаем файл с музыкой
+    pygame.mixer.music.load('data/music/Mario.mp3')  # подгружаем файл с музыкой
     pygame.mixer.music.play()  # включаем музыку
     intro_text = [
         "ЗАСТАВКА",
@@ -81,6 +150,9 @@ def start_screen():
 
     while True:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    terminate()
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
@@ -97,7 +169,8 @@ def load_level(filename):
         with open(filename, "r") as mapFile:
             level_map = [line.strip() for line in mapFile]
             # и подсчитываем максимальную длину
-        max_width = max(map(len, level_map))
+        # max_width = max(map(len, level_map))
+        max_width = 32
         # дополняем каждую строку пустыми клетками ('.')
         return list(map(lambda x: x.ljust(max_width, "."), level_map))
     except FileNotFoundError:
@@ -106,139 +179,148 @@ def load_level(filename):
         terminate()
 
 
-def generate_level(level):
-    new_player, x, y = None, None, None
+def generate_level(level):  # генерируем спрайты карты каждую клетку
+    new_player, x, y, door = None, None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == ".":
                 Tile("empty", x, y)
             elif level[y][x] == "#":
                 Tile("wall", x, y)
+            elif level[y][x] == '+':
+                Tile("empty", x, y)
+                Coin('coin.png', x, y)
+            elif level[y][x] == 'c':
+                Tile("doorclose", x, y)
+                door = (x, y)
             elif level[y][x] == "@":
                 Tile("empty", x, y)
                 new_player = Player(x, y)
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-def chioce_level_screen():
-    list_file = [
-        file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
-    ]
-    choice_level = list_file[0]
-
-    fon = pygame.transform.scale(load_image("fon.png"), (width, height))
-    screen.blit(fon, (0, 0))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return choice_level  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
+    return new_player, x, y, door
 
 
 # Код самой игры
-
-# Отображение имен файлов с уровнями игры для предварительного просмотра и выбора нужного
-'''
-list_file = [
-    file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
-]
-if list_file:
-    print(
-        "Имена файлов с доступными для загрузки уровнями:"
-    )  # для отображения файлы с уровнями игры должны именоваться level*.txt
-    for f in list_file:
-        print(f)
-
-#level = input("Введите название файла в котором расположена карта уровня: ")
-level = 'level.txt'
-'''
-
+score = 0  # общий счёт игры
+lives = 3  # количество жизней на старте
 pygame.init()
 pygame.key.set_repeat(200, 70)  # контроль повторения удерживаемых клавиш
 FPS = 50
 # Размер окна игры 1280*720 размер клетки 40*40
 step = tile_width = tile_height = 40
-width = 32 * tile_width
-height = 18 * tile_height
-
+width = 32 * tile_width  # 32 клетки в ширину
+height = 18 * tile_height  # 18 клеток в высоту
+image_level_height = 55  # высота для спрайтов "выбора уровня"
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-
+start_screen()  # Заставка перед началом игры
 # основной персонаж
 player = None
-
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-
+levels_group = pygame.sprite.Group()  # Группа спрайтов для отображения уровней
+coin_group = pygame.sprite.Group()  # Группа спрайтов монет
 # Запуск игры
-tile_images = {"wall": load_image("box.png"), "empty": load_image("grass.png")}
+
+# список уровней
+list_file = [
+    file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
+]
+# изображения для спрайтов уровней
+level_images = dict()
+for i in list_file:
+    level_images[i[:-4]] = load_image(i[:-4] + '.png')
+image_gen_level(level_images)  # добавляем спрайты уровней
+
+tile_images = {"wall": load_image("box.png"), "empty": load_image("grass.png"),
+               "doorclose": load_image("doorclose.png"), "dooropen": load_image("dooropen.png")}
 player_image = load_image("mario.png", -1)
+chioce_level_num = chioce_level_screen()  # номер стартового уровня
+while chioce_level_num <= 8:  # цикл проигрывания уровня
+    level_name = list_file[chioce_level_num]  # Выбор уровня игры level = filename 'level.txt'
+    level_map = load_level(level_name)  # карта уровня список строк
+    # положение игрока и размер карты
+    player, level_x, level_y, door_level = generate_level(level_map)
+    # подсчет количества монет в текущем уровне
+    count_coin_cur_level = sum(s.count('+') for s in level_map)
+    level_end = False
+    dx = player.rect.x
+    dy = player.rect.y
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                #running = False
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                   # running = False
+                    terminate()
+                if event.key == pygame.K_LEFT:
+                    if (player.rect.x - step) < 0 or dx < 40 or \
+                            level_map[(player.rect.y) // step] \
+                                    [(player.rect.x - 8) // step - 1] == '#':
+                        continue
+                    player.rect.x -= step
+                    dx -= step
+                if event.key == pygame.K_RIGHT:
+                    if (player.rect.x + step) > 1280 or dx > 1240 or \
+                            level_map[(player.rect.y) // step] \
+                                    [(player.rect.x - 8) // step + 1] == '#':
+                        continue
+                    player.rect.x += step
+                    dx += step
+                if event.key == pygame.K_UP:
+                    if (player.rect.y - step) < 0 or dy < 40 or level_map[(player.rect.y) // step - 1] \
+                            [(player.rect.x - 8) // step] == '#':
+                        continue
+                    player.rect.y -= step
+                    dy -= step
+                if event.key == pygame.K_DOWN:
+                    if (player.rect.y + step) > 720 or dy > 680 or \
+                            level_map[(player.rect.y) // step + 1] \
+                                    [(player.rect.x - 8) // step] == '#':
+                        continue
+                    player.rect.y += step
+                    dy += step
+        screen.fill(pygame.Color(0, 0, 0))
+        tiles_group.draw(screen)
+        coin_group.draw(screen)
+        player_group.draw(screen)
+        # вывод общего счета в игре  и количества оставшихся жизней
+        title_level = 'SCORE: ' + str(score) + ' '* (20 - len(str(score))) + 'LIVES: ' + str(lives)
+        font = pygame.font.Font(None, 40)
+        string_rendered = font.render(title_level, 1, pygame.Color("black"))
+        intro_rect = string_rendered.get_rect()
+        intro_rect.x = 10
+        intro_rect.y = 10
+        screen.blit(string_rendered, intro_rect)
+        # вывод общего счета окончание  и количества оставшихся жизней
 
-camera = Camera()
+        pygame.display.flip()
+        all_sprites.update()  # обновление
+        clock.tick(FPS)  # обновление экрана
+        coin_pickup = pygame.sprite.spritecollide(player, coin_group, True)  # проверяем столкновение с монетой
+        if coin_pickup:
+            count_coin_cur_level -= 1 # увеличиваем количество подобранных монет
+            score += 1 # увеличиваем общий счет
+            pygame.mixer.music.load('data/music/coin.mp3')  # подгружаем файл с музыкой
+            pygame.mixer.music.play()  # включаем музыку
+        if count_coin_cur_level == 0:  # уровень завершен
+            level_end = True
+            Tile("dooropen", *door_level)
+        if level_end and ((player.rect.x - 8) // step, player.rect.y // step) == door_level:  #  уровень пройден
+            pygame.mixer.music.load('data/music/next_level.mp3')  # подгружаем файл с музыкой
+            pygame.mixer.music.play()  # включаем музыку
+            if chioce_level_num < 8:
+                chioce_level_num += 1
+                player.kill() #удаляем игрока с поля
+                running = False
+            else:
+                # end_game
+                terminate()
 
-start_screen()  # Заставка перед началом игры
-level = chioce_level_screen()  # Выбор уровня игры
-# положение игрока и размер карты
-player, level_x, level_y = generate_level(load_level(level))
-# Inna
-dx = -(player.rect.x + player.rect.w // 2 - width // 2)
-dy = -(player.rect.y + player.rect.h // 2 - height // 2)
-
-# Реализовать выбор уровня
-
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                if player.rect.x - dx < step or \
-                        load_level(level)[(player.rect.y - dy) // step] \
-                                [(player.rect.x - dx) // step - 1] == '#':
-                    continue
-                player.rect.x -= step
-                dx += step
-                # player.rect.x -= step
-            if event.key == pygame.K_RIGHT:
-                if (player.rect.x - dx) // step == level_x or \
-                        load_level(level)[(player.rect.y - dy) // step] \
-                                [(player.rect.x - dx) // step + 1] == '#':
-                    continue
-                player.rect.x += step
-                dx -= step
-                # player.rect.x += step
-            if event.key == pygame.K_UP:
-                if player.rect.y - dy < step or load_level(level)[(player.rect.y - dy) // step - 1] \
-                        [(player.rect.x - dx) // step] == '#':
-                    continue
-                player.rect.y -= step
-                dy += step
-                # player.rect.y -= step
-            if event.key == pygame.K_DOWN:
-                if (player.rect.y - dy) // step == level_y or \
-                        load_level(level)[(player.rect.y - dy) // step + 1] \
-                                [(player.rect.x - dx) // step] == '#':
-                    continue
-                player.rect.y += step
-                dy -= step
-                # player.rect.y += step
-
-    camera.update(player)
-    for sprite in all_sprites:
-        camera.apply(sprite)
-
-    screen.fill(pygame.Color(0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    pygame.display.flip()
-    clock.tick(FPS)  # обновление экрана
 
 terminate()
